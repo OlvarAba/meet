1b145b96a32b36f7e561ac7d711166a8e5938920
Olvar Abadeen <oabadeen@gmail.com>
2022-11-10 12:02:31 -0800

    4.7 Added Alerts

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,46 +1,45 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
     state = {
-      numberOfEvents: 32,
-      errorText: '',
+        numberOfEvents: 32,
+        infoText: '',
     };
-  
+
     handleInputChanged = (event) => {
-      const value = event.target.value;
-      if (value < 1 || value > 32) {
+      let inputValue = event.target.value;
+      if (inputValue >= 33 || inputValue <= 0) {
         this.setState({
-          numberOfEvents: value,
-          errorText: 'Enter number from 1 to 32',
+          numberOfEvents: inputValue,
+          infoText: 'Please enter a number between 1 - 32.',
         });
       } else {
         this.setState({
           numberOfEvents: event.target.value,
-          errorText: '',
+          infoText: ' ',
         });
       }
-  
-      this.props.updateEvents(undefined, value);
+
+      this.props.updateEvents(undefined, inputValue);
     };
-  
+
     render() {
-      return (
-        <div>
-          <div className="number-of-events">
-            <label>
-              Number of Events:
-              <input
-                type="number"
-                className="number-of-events-input"
-                min="1"
-                value={this.state.numberOfEvents}
-                onChange={this.handleInputChanged}
-              />
-            </label>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  export default NumberOfEvents;
+        return (
+            <div className="numberOfEvents">
+                <label htmlFor="number-of-events">Show max: </label>
+                <input
+                  type="number"
+                  id="events-num-input"
+                  value={this.state.numberOfEvents}
+                  onChange={this.handleInputChanged}
+                />
+                <div>
+                  <ErrorAlert text={this.state.infoText} />
+                </div>
+              </div>
+            );
+          }
+        }
+
+export default NumberOfEvents;
