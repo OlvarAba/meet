ab893adaaa54a0f6d2a1aa21d23cad03cc677847
Olvar Abadeen <oabadeen@gmail.com>
2022-11-13 21:26:19 -0800

    added event component

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = {
+    event: {},
+    collapsed: true,
+  };
+
+  handleClick = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
   render() {
-    return <div></div>;
+    const { event } = this.props;
+    const { collapsed } = this.state;
+
+    console.log(event);
+    return (
+      <div className="event">
+        <h5 className="summary">{event.summary}</h5>
+        <div className="event-body">
+          <p className="location">
+            @{event.summary} | {event.location}
+          </p>
+
+          {!collapsed && (
+            <div
+              className={`extra-details ${
+                this.state.collapsed ? "hide" : "show"
+              }`}
+            >
+              <br />
+              <p className="event-description">{event.description}</p>
+            </div>
+          )}
+
+          <button
+            variant="primary"
+            id="eventDetails"
+            className={`${collapsed ? "show" : "hide"}-details-btn`}
+            onClick={this.handleClick}
+          >
+            {collapsed ? "Show Details" : "Hide Details"}
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 export default Event;
